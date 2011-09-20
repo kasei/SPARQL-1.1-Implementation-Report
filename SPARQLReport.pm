6cdb380e385f72ad9078179a9812f9c71317e552
Gregory Todd Williams <greg@evilfunhouse.com>
2011-09-20 12:32:55 -0400

    Refactored implementation report code.

---
+++ SPARQLReport.pm
@@ -0,0 +1,311 @@
+package SPARQLReport;
+
+use strict;
+use warnings;
+use v5.12;
+
+use File::Spec;
+use RDF::Redland;
+use RDF::Trine qw(iri);
+use RDF::Trine::Namespace qw(foaf rdfs);
+use RDF::Query;
+use RDF::Trine::Error qw(:try);
+use Scalar::Util qw(blessed);
+use HTML::Entities;
+
+my $manifestbase	= 'http://www.w3.org/2009/sparql/docs/tests/data-sparql11';
+my $doap			= RDF::Trine::Namespace->new( 'http://usefulinc.com/ns/doap#' );
+my $mf				= RDF::Trine::Namespace->new( 'http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#' );
+my $dt				= RDF::Trine::Namespace->new( 'http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#' );
+
+sub new {
+	my $class			= shift;
+	my $sources			= shift || [];
+	my $manifestdir		= shift;
+	my $self			= bless({ sources => $sources, manifestdir => $manifestdir }, $class);
+	$self->{'exists'}	= (-r 'sparql.sqlite');
+	my $store			= RDF::Trine::Store::DBI::SQLite->new('model', 'dbi:SQLite:dbname=sparql.sqlite', '', '');
+	$self->{model}		= RDF::Trine::Model->new( $store );
+	return $self;
+}
+
+sub sources {
+	my $self	= shift;
+	return @{ $self->{ sources } };
+}
+
+sub model {
+	my $self	= shift;
+	return $self->{model};
+}
+
+sub manifestdir {
+	my $self	= shift;
+	return $self->{manifestdir};
+}
+
+sub load_data {
+	my $self	= shift;
+	my $model			= $self->model;
+	my $parser			= RDF::Trine::Parser->new('turtle');
+	
+	my $man				= File::Spec->catfile( $self->manifestdir, 'manifest-all.ttl' );
+	for my $f ($man) {
+		warn "# loading $f\n";
+		try {
+			my $base	= join('/', $manifestbase, 'manifest-all.ttl');
+			$parser->parse_file_into_model( $base, $f, $model, context => iri('http://myrdf.us/ns/sparql/Manifests') );
+		} catch Error with {
+			my $e	= shift;
+		};
+	}
+	
+	$self->get_manifests();
+	$self->get_software();
+	$self->get_test_status();
+	$self->get_test_results();
+	unless ($self->{'exists'}) {
+		$self->load_manifest_data;
+		$self->load_source_data;
+	}
+}
+
+sub load_manifest_data {
+	my $self	= shift;
+	my $model	= $self->model;
+	my $parser			= RDF::Trine::Parser->new('turtle');
+	foreach my $f ($self->manifests) {
+		try {
+			my ($dir)	= ($f =~ m{([^/]+)/manifest.ttl});
+			my $base	= join('/', $manifestbase, $dir, 'manifest.ttl' );
+			my $file	= File::Spec->catfile( $self->manifestdir, $f );
+			warn "# loading $file with base $base\n";
+			$parser->parse_file_into_model( $base, $file, $model, context => iri('http://myrdf.us/ns/sparql/Manifests') );
+		} catch Error with {
+			my $e	= shift;
+		};
+	}
+}
+
+sub load_source_data {
+	my $self	= shift;
+	my $model	= $self->model;
+	my $parser	= RDF::Trine::Parser->new('turtle');
+	foreach my $u ($self->sources) {
+		warn "# loading $u\n";
+		try {
+			RDF::Trine::Parser->parse_url_into_model( $u, $model, context => iri('http://myrdf.us/ns/sparql/Implementations') );
+		} catch Error with {
+			my $e	= shift;
+			warn $e->text;
+		};
+	}
+}
+
+sub get_manifests {
+	my $self	= shift;
+	my $model	= $self->model;
+	{
+		my $iter	= $model->get_statements( undef, $mf->conformanceRequirement );
+		my %manifests;
+		my %specs;
+		while (my $st = $iter->next) {
+			my $spec	= $st->subject;
+			my $uri		= $spec->uri_value;
+			my $list	= $st->object;
+			my @mans;
+			foreach my $n ($model->get_list( $list )) {
+				my $u	= $n->uri_value;
+				my $f	= $u;
+				$f		=~ s[$manifestbase/][];
+				$manifests{ files }{ $f }++;
+				$manifests{ specs }{ $f }{ $uri }++;
+				push(@mans, $f);
+			}
+			my ($name)	= $uri =~ m{([^/]+)/*$};
+			$specs{ $uri }{ manifests }	= { map { $_ => 1 } @mans };
+			$specs{ $uri }{ id }		= $name;
+		}
+		$self->{specs}		= \%specs;
+		$self->{manifests}	= \%manifests;
+	}
+	foreach my $s (keys %{ $self->{specs} }) {
+		my $spec	= iri($s);
+		my ($name)	= $model->objects( $spec, $rdfs->label, undef );
+		if ($name) {
+			$self->{ specs }{ $s }{ name }	= $name->literal_value;
+		} else {
+			$self->{ specs }{ $s }{ name }	= $self->{ specs }{ $s }{ id };
+		}
+	}
+}
+
+sub specs {
+	my $self	= shift;
+	return keys %{ $self->{ specs } };
+}
+
+sub spec_name {
+	my $self	= shift;
+	my $spec	= shift;
+	return $self->{specs}{ $spec }{ name };
+}
+
+sub spec_id {
+	my $self	= shift;
+	my $spec	= shift;
+	return $self->{specs}{ $spec }{ id };
+}
+
+sub manifests {
+	my $self	= shift;
+	my @manifests	= keys %{ $self->{ manifests }{ files } };
+	return @manifests;
+}
+
+sub spec_manifests {
+	my $self	= shift;
+	my $spec	= shift;
+	my $specs	= $self->{specs};
+	return keys %{ $specs->{ $spec }{ manifests } }
+}
+
+sub manifest_iri {
+	my $self	= shift;
+	my $man		= shift;
+	return iri(join('/',$manifestbase,$man));
+}
+
+sub spec_tests {
+	my $self		= shift;
+	my $spec		= shift;
+	my $model		= $self->model;
+	my ($man_list)	= $model->objects( iri($spec), $mf->conformanceRequirement );
+	my @man			= $model->get_list( $man_list );
+	my @tests		= map { $self->manifest_tests( $_ ) } @man;
+	return sort { $a->uri_value cmp $b->uri_value } @tests;
+}
+
+sub manifest_tests {
+	my $self		= shift;
+	my $man			= shift;
+	my $model		= $self->model;
+	my ($test_list)	= $model->objects( $man, $mf->entries );
+	my @tests		= $model->get_list( $test_list );
+	return sort { $a->uri_value cmp $b->uri_value } @tests;
+}
+
+sub test_is_optional {
+	my $self	= shift;
+	my $test	= shift;
+	my $iri		= $test->uri_value;
+	my @groups	= keys %{ $self->{ test_status }{ $iri } || {} };
+	return scalar(@groups) ? 1 : 0;
+}
+
+sub test_approval_status {
+	my $self	= shift;
+	my $test	= shift;
+	my $iri		= $test->uri_value;
+	return $self->{ test_approval }{ $iri }
+}
+
+sub software_test_result {
+	my $self		= shift;
+	my $software	= shift;
+	my $test		= shift;
+	my $model		= $self->model;
+	return $self->{ test_results }{ software }{ $software->uri_value }{ $test->uri_value };
+}
+
+sub get_test_status {
+	my $self	= shift;
+	my $model	= $self->model;
+	{
+		my $iter	= $model->get_statements( undef, $mf->requires );
+		while (my $st = $iter->next) {
+			my $test	= $st->subject->uri_value;
+			my $req		= $st->object->uri_value;
+			$self->{ test_status }{ $test }{ $req }++;
+			$self->{ require_groups }{ $req }{ $test }++;
+		}
+	}
+	
+	{
+		my $iter	= $model->get_statements( undef, $dt->approval );
+		while (my $st = $iter->next) {
+			my $test	= $st->subject->uri_value;
+			my $status	= $st->object->uri_value;
+			$self->{ test_approval }{ $test }	= $status;
+		}
+	}
+	
+	
+}
+
+sub get_test_results {
+	my $self	= shift;
+	my $model	= $self->model;
+	my $query	= RDF::Query->new(<<"END");
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX earl: <http://www.w3.org/ns/earl#>
+PREFIX dt: <http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#>
+PREFIX mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#>
+SELECT ?test ?outcome ?approval ?software
+WHERE {
+	GRAPH <http://myrdf.us/ns/sparql/Implementations> {
+		[]
+			earl:test ?test ;
+			earl:result [ earl:outcome ?outcome ] ;
+			earl:subject ?software .
+	}
+}
+END
+	my $iter	= $query->execute( $model );
+	while (my $r = $iter->next) {
+		my $test		= $r->{test}->uri_value;
+		my $outcome		= $r->{outcome}->uri_value;
+		my $software	= $r->{software}->uri_value;
+		push( @{ $self->{ test_results }{raw} }, [$test, $outcome, $software] );
+		$self->{ test_results }{ software }{ $software }{ $test }	= $outcome;
+		$self->{ test_results }{ test }{ $test }{ $software }		= $outcome;
+	}
+}
+
+sub get_software {
+	my $self	= shift;
+	my $model	= $self->model;
+	my %software;
+	{
+		my $iter	= $model->get_statements( undef, iri('http://www.w3.org/ns/earl#subject'), undef, iri('http://myrdf.us/ns/sparql/Implementations') );
+		while (my $st = $iter->next) {
+			my $s	= $st->object->uri_value;
+			$software{ $s }++;
+		}
+	}
+	$self->{software}	= [ map { iri($_) } keys %software ];
+	
+	foreach my $s (@{ $self->{software} }) {
+# 		push(@names, $model->objects( 
+		my @names	= grep { blessed($_) and $_->isa('RDF::Trine::Node::Literal') } $model->objects_for_predicate_list( $s, $doap->name, $foaf->name, $rdfs->label );
+		if (@names) {
+			$self->{ software_names }{ $s->uri_value }	= $names[0]->literal_value;
+		} else {
+			$self->{ software_names }{ $s->uri_value }	= $s->uri_value;
+		}
+	}
+}
+
+sub software {
+	my $self	= shift;
+	return @{ $self->{software} };
+}
+
+sub software_name {
+	my $self	= shift;
+	my $s		= shift;
+	my $uri		= $s->uri_value;
+	return $self->{ software_names }{ $uri };
+}
+
+1;
--- status.pl
+++ status.pl
@@ -3,66 +3,14 @@
 use strict;
 use warnings;
 no warnings 'uninitialized';
-
+use lib qw(. lib);
 use Data::Dumper;
-use File::Spec;
-use RDF::Redland;
-use RDF::Trine qw(iri);
-use RDF::Trine::Namespace qw(foaf rdfs);
-use RDF::Query;
-use RDF::Trine::Error qw(:try);
-use Scalar::Util qw(blessed);
-use HTML::Entities;
-
+use SPARQLReport;
 
 my $url_file		= shift || 'reports.txt';
 my $manifestdir		= shift || '/Users/samofool/data/prog/git/perlrdf/RDF-Query/xt/dawg11';
-my $manifestbase	= 'http://www.w3.org/2009/sparql/docs/tests/data-sparql11';
-my $doap			= RDF::Trine::Namespace->new( 'http://usefulinc.com/ns/doap#' );
-my $mf				= RDF::Trine::Namespace->new( 'http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#' );
+
 my $date			= scalar(gmtime) . ' GMT';
-my $exists			= (-r 'sparql.sqlite');
-my $store			= RDF::Trine::Store::DBI::SQLite->new('model', 'dbi:SQLite:dbname=sparql.sqlite', '', '');
-my $model			= RDF::Trine::Model->new( $store );
-my $parser			= RDF::Trine::Parser->new('turtle');
-
-#my @manifests		= glob( "${manifestdir}/*/manifest.ttl" );
-
-my $man				= File::Spec->catfile( $manifestdir, 'manifest-all.ttl' );
-for my $f ($man) {
-	warn "# loading $f\n";
-	try {
-		my $base	= join('/', $manifestbase, 'manifest-all.ttl');
-		$parser->parse_file_into_model( $base, $f, $model );
-	} catch Error with {
-		my $e	= shift;
-	};
-}
-
-my (@specs, %specs, @manifests);
-{
-	my %manifests;
-	my $iter	= $model->get_statements( undef, $mf->conformanceRequirement );
-	while (my $st = $iter->next) {
-		my $spec	= $st->subject;
-		my $uri		= $spec->uri_value;
-		my $list	= $st->object;
-		my @mans;
-		foreach my $n ($model->get_list( $list )) {
-			my $u	= $n->uri_value;
-			my $f	= $u;
-			$f		=~ s[$manifestbase/][];
-			$manifests{ files }{ $f }++;
-			$manifests{ specs }{ $f }{ $uri }++;
-			push(@mans, $f);
-		}
-		my ($name)	= $uri =~ m{([^/]+)/*$};
-		$specs{ $uri }{ manifests }	= { map { $_ => 1 } @mans };
-		$specs{ $uri }{ name }		= $name;
-	}
-	@specs		= keys %specs;
-	@manifests	= keys %{ $manifests{ files } };
-}
 
 my @sources;
 open( my $fh, '<:utf8', $url_file ) or die $!;
@@ -74,290 +22,146 @@ while (defined(my $u = <$fh>)) {
 }
 close($fh);
 
-unless ($exists) {
-	foreach my $f (@manifests) {
-		try {
-			my ($dir)	= ($f =~ m{([^/]+)/manifest.ttl});
-			my $base	= join('/', $manifestbase, $dir, 'manifest.ttl' );
-			my $file	= File::Spec->catfile( $manifestdir, $f );
-			warn "# loading $file with base $base\n";
-			$parser->parse_file_into_model( $base, $file, $model );
-		} catch Error with {
-			my $e	= shift;
-		};
-	}
-	foreach my $u (@sources) {
-		warn "# loading $u\n";
-		try {
-			RDF::Trine::Parser->parse_url_into_model( $u, $model );
-		} catch Error with {
-			my $e	= shift;
-			warn $e->text;
-		};
-	}
-}
+my $r	= SPARQLReport->new( \@sources, $manifestdir );
+$r->load_data();
 
 
-# my $iter	= $model->get_statements( undef, $mf->entries );
-# while (my $r = $iter->next) {
-# 	warn $r->as_string;
-# }
-# exit;
 
-foreach my $spec (@specs) {
+# foreach my $spec ($r->specs) {
 # 	warn "*** spec $spec\n";
-	foreach my $man (keys %{ $specs{ $spec }{ manifests } }) {
+# 	foreach my $man ($r->spec_manifests( $spec )) {
 # 		warn "      manifest $man\n";
-		my $m	= iri(join('/',$manifestbase,$man));
-		my ($test_list)	= $model->objects( $m, $mf->entries );
-		my @tests		= $model->get_list( $test_list );
-		foreach my $test (@tests) {
+# 		my $m		= $self->manifest_iri( $man );
+# 		my @tests	= $r->manifest_tests( $m );
+# 		foreach my $test (@tests) {
 # 			warn "        test $test\n";
-			$specs{ $spec }{ tests }{ $test->uri_value }++;
-		}
+# # 			$specs{ $spec }{ tests }{ $test->uri_value }++;
+# 		}
+# 	}
+# }
+
+my %software_ids;
+foreach my $s ($r->software) {
+	my $id	= lc($r->software_name( $s ));
+	$id		=~ s/[^a-zA-Z0-9]/-/g;
+	$id		=~ s/--+/-/g;
+	$id		=~ s/^-+//;
+	$id		=~ s/-+$//;
+	$software_ids{ $s }	= "impl-$id";
+}
+
+
+print_html_head($r);
+
+
+# warn "Summary\n";
+foreach my $s ($r->software) {
+	my $software	= $s->uri_value;
+# 	warn "\t$software\n";
+	foreach my $spec ($r->specs) {
+# 		warn "\t\t$spec results...\n";
+# 		print table cell with software-spec conformance
 	}
 }
 
-my %requirements	= get_requirements( $model );
-my @software		= get_software( $model );
-my @slist			= reverse sort @specs;
-print_html_head(\@slist, \%specs);
-foreach my $spec (@slist) {
-	print_spec_test_table($model, $spec, \%specs, \@software, \%requirements);
+my $width	= 2 + scalar(@{ [ $r->software ] });
+
+# warn "Specs\n";
+print qq[<table>\n];
+foreach my $spec ($r->specs) {
+# 	warn "\t$spec\n";
+	my $specname	= $r->spec_name( $spec );
+	my $specid		= $r->spec_id( $spec );
+	print <<"END";
+	<tr><th colspan="${width}"><h3 id="$specid">$specname</h3></th></tr>
+	<tr>
+		<th>Test</th>
+		<th>Status</th>
+END
+	foreach my $s ($r->software) {
+		my $software	= $s->uri_value;
+		my $name		= $r->software_name( $s );
+		print "\t\t<th>$name</th>\n";
+	}
+	print "\t</tr>\n";
+	
+	my $total	= 0;
+	my %software_totals;
+	my %software_passes;
+	foreach my $t ($r->spec_tests( $spec )) {
+		$total++;
+		next if ($r->test_is_optional( $t ));
+		my $iri		= $t->uri_value;
+		my $name	= strip_test($iri);
+		my $status	= $r->test_approval_status( $t );
+		$status	=~ s{http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#}{};
+# 		warn "\t\t" . $t->uri_value . "\n";
+		print <<"END";
+	<tr>
+		<td>$name</td>
+		<td class="${status}">${status}</td>
+END
+		foreach my $software ($r->software) {
+			my $sid	= $software_ids{ $software };
+			my $outcome	= $r->software_test_result( $software, $t );
+			if ($outcome) {
+				$outcome	=~ s{http://www.w3.org/ns/earl#}{};
+				$software_totals{ $software }++;
+				$software_passes{ $software }++ if ($outcome eq 'pass');
+				print qq[\t\t<td class="$sid ${outcome}">${outcome}</td>\n];
+			} else {
+				print qq[\t\t<td class="$sid notrun">not run</td>\n];
+			}
+		}
+		print "\t</tr>\n";
+	}
+
+
+	print qq[<tr><td colspan="2">Total &mdash; %Total/%Run (Pass/Run/Total)</td>\n];
+	foreach my $software ($r->software) {
+		my $sid		= $software_ids{ $software };
+		my $run		= $software_totals{ $software } || 0;
+		my $pass	= $software_passes{ $software } || 0;
+		my $rperc	= ($run == 0) ? 'n/a' : sprintf('%.1f%%', 100*($pass/$run));
+		my $tperc	= ($total == 0) ? 'n/a' : sprintf('%.1f%%', 100*($pass/$total));
+		print qq[\t<td class="$sid"><span title="($pass/$run/$total)">$tperc/$rperc</span></td>\n];
+	}
+	print qq[</tr>\n];
+
+# 	print totals of required software-test conformance
+# 	foreach optional test group in spec
+# 		foreach optional test
+# 			print row header cells with test name and approval status
+# 			foreach software
+# 				print table cell with software-test conformance
+# 		print totals of optional group software-test conformance
 }
-print qq[<p>Sources:</p>\n<ul>\n];
-foreach my $u (@sources) {
-	print qq[<li><a href="$u">] . encode_entities($u) . qq[</a></li>\n];
+
+print qq[</table>\n];
+
+print "<p>Sources:</p>\n<ul>\n";
+foreach my $source (@sources) {
+	print "\t<li>$source</li>\n";
 }
-print qq[</ul>\n];
+print "</ul>\n";
+
+print qq[<p class="foot">$date</p>\n];
+
 print_html_foot();
-
-
-
 
 
 ################################################################################
 
-
-
-sub get_requirements {
-	my $model	= shift;
-	my %requirements;
-	warn "# getting test requirements\n";
-	my $iter	= $model->get_statements( undef, $mf->requires, undef );
-	while (my $st = $iter->next) {
-		my $test	= $st->subject->uri_value;
-		my $req		= $st->object->uri_value;
-		if (exists( $requirements{tests}{ $test } )) {
-			my $req1	= $requirements{tests}{ $test };
-			warn "Test $test has two requirements: $req1, $req\n";
-		}
-		$requirements{tests}{ $test }	= $req;
-		push(@{ $requirements{groups}{$req} }, $test);
-	}
-	return %requirements;
-}
-
-sub get_software {
-	my $model	= shift;
-	my @software;
-	my $squery	= RDF::Query->new('SELECT DISTINCT ?software WHERE { [] <http://www.w3.org/ns/earl#subject> ?software }');
-	my $siter	= $squery->execute( $model );
-	while (my $r = $siter->next) {
-		my $s	= $r->{software}->uri_value;
-		push(@software, $s);
-	}
-	return @software;
-}
-
-sub get_results {
-	my $model			= shift;
-	my $requirements	= shift;
-	my $specs			= shift;
-	my $software		= shift;
-	my %conditions		= @_;
-	my %results;
-	my %software;
-	warn "# getting EARL results for " . join(' ', %conditions) . "\n";
-	my $query	= RDF::Query->new(<<"END");
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX earl: <http://www.w3.org/ns/earl#>
-PREFIX dt: <http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#>
-PREFIX mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#>
-SELECT ?test ?outcome ?approval ?software
-WHERE {
-	[]
-		earl:test ?test ;
-		earl:result ?result ;
-		earl:subject ?software .
-	?result earl:outcome ?outcome .
-	?test dt:approval ?approval .
-}
-END
-	my $iter	= $query->execute( $model );
-	while (my $row = $iter->next) {
-		my ($t, $o, $a, $s)	= map { (blessed($_) and $_->can('uri_value')) ? $_->uri_value : $_ } @{ $row }{ qw(test outcome approval software) };
-		my $test	= $t;
-		$t	= strip_test($t);
-		$a	=~ s{http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#}{};
-		$o	=~ s{http://www.w3.org/ns/earl#}{};
-		
-		if (my $spec = $conditions{ spec }) {
-			# skip the test unless it's defined for this spec
-			next unless $specs->{ $spec }{ tests }{ $test };
-		}
-		
-		my $group;
-		if (my $req = $requirements->{tests}{$test}) {
-			$group	= 'optional';
-			$results{ software }{ $s }{ optional }{ $req }{ total }++;
-			$results{ software }{ $s }{ optional }{ $req }{ pass }++ if ($o eq 'pass');
-			$results{ tests }{ $group }{ $req }{ $test }{ run }++;
-		} else {
-			$group	= 'required';
-			$results{ software }{ $s }{ required }++;
-			$results{ software }{ $s }{ required_pass }++ if ($o eq 'pass');
-		}
-		
-		$results{ tests }{ $group }{ $t }{ approval }		= encode_entities($a);
-		$results{ tests }{ $group }{ $t }{ software }{ $s }	= encode_entities($o);
-		$results{ software }{ $s }{ total }++;
-		$results{ software }{ $s }{ total_pass }++ if ($o eq 'pass');
-	}
-	
-	
-	foreach my $s (@$software) {
-		my @names	= grep { blessed($_) and $_->isa('RDF::Trine::Node::Literal') } $model->objects_for_predicate_list( iri($s), $doap->name, $foaf->name, $rdfs->label );
-		if (@names) {
-			$software{ $s }{ name }	= encode_entities($names[0]->literal_value);
-		} else {
-			$software{ $s }{ name }	= encode_entities($s);
-		}
-		
-		my @h			= $model->objects_for_predicate_list( iri($s), $doap->homepage );
-		my @homepages	= grep { blessed($_) and $_->isa('RDF::Trine::Node::Resource') } @h;
-		if (@homepages) {
-			my $h		= $homepages[0]->uri_value;
-			my $name	= $software{ $s }{ name };
-			$software{ $s }{ name }	= qq[<a href="$h">$name</a>];
-		}
-	}
-	return (\%results, \%software);
-}
-
-sub print_spec_test_table {
-	my $model		= shift;
-	my $spec		= shift;
-	my $specs		= shift;
-	my $sl			= shift;
-	my $requirements	= shift;
-	my ($r, $sh)	= get_results( $model, $requirements, $specs, $sl, spec => $spec );
-	
-	my %results		= %$r;
-	my @software	= @$sl;
-	my %software	= %$sh;
-	my $name		= $specs{ $spec }{ name };
-
-
-
-
-	print <<"END";
-<h3 id="$name">$name</h3>
-<table>
-<tr>
-	<th>Test</th>
-	<th>Status</th>
-END
-
-	my $columns	= 2 + scalar(@software);
-	foreach my $s (@software) {
-# 		warn "# software $s\n";
-		my $name	= $software{ $s }{ name };
-		print qq[\t<th>$name</th>\n];
-	}
-	
-	print <<"END";
-</tr>
-END
-
-	{
-		print qq[<tr><th colspan="$columns">Required Tests</td></tr>\n];
-		foreach my $t (sort keys %{ $results{ tests }{ required } }) {
-# 			warn "# test $t\n";
-			my $a	= $results{ tests }{ required }{ $t }{ approval };
-			print qq[<tr>\n\t<td>$t</td>\n\t<td class="$a">$a</td>\n];
-			foreach my $s (@software) {
-				my $o	= $results{ tests }{ required }{ $t }{ software }{ $s };
-				if ($o) {
-					print qq[<td class="$o">$o</td>\n];
-				} else {
-					print qq[<td>not run</td>\n];
-				}
-			}
-			print qq[</tr>\n];
-		}
-		print qq[<tr><td colspan="2">Total &mdash; %Total/%Run (Pass/Run/Total)</td>\n];
-		foreach my $s (@software) {
-			my ($total, $run, $pass)	= get_spec_totals_for_software( \%results, $s );
-			my $rperc	= ($run == 0) ? 'n/a' : sprintf('%.1f%%', 100*($pass/$run));
-			my $tperc	= ($total == 0) ? 'n/a' : sprintf('%.1f%%', 100*($pass/$total));
-			print qq[\t<td><span title="($pass/$run/$total)">$tperc/$rperc</span></td>\n];
-		}
-		print qq[</tr>\n];
-	}
-	
-	
-	foreach my $req (sort keys %{ $requirements->{groups} }) {
-		my $name	= $req;
-		$name		=~ s{http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#}{};
-
-		my @opt_tests	= grep { exists $results{ tests }{ optional }{ $req }{ $_ }{ run } } @{ $requirements->{groups}{$req} };
-		if (scalar(@opt_tests)) {
-			print qq[<tr><th colspan="$columns">Optional Tests: $name</td></tr>\n];
-			my $total	= scalar(@opt_tests);
-			foreach my $t (sort @{ $requirements->{groups}{$req} }) {
-				$t	= strip_test($t);
-	# 			warn "# optional test $t\n";
-				my $a	= $results{ tests }{ optional }{ $t }{ approval };
-				print qq[<tr>\n\t<td>$t</td>\n\t<td class="$a">$a</td>\n];
-				foreach my $s (@software) {
-					my $o	= $results{ tests }{ optional }{ $t }{ software }{ $s };
-					if ($o) {
-						print qq[<td class="$o">$o</td>\n];
-					} else {
-						print qq[<td>not run</td>\n];
-					}
-				}
-				print qq[</tr>\n];
-			}
-			print qq[<tr><td colspan="2">Total &mdash; %Total/%Run (Pass/Run/Total)</td>\n];
-			foreach my $s (@software) {
-				my $run		= $results{ software }{ $s }{ optional }{ $req }{ total } || 0;
-				my $pass	= $results{ software }{ $s }{ optional }{ $req }{ pass } || 0;
-				my $rperc	= ($run == 0) ? 'n/a' : sprintf('%.1f%%', 100*($pass/$run));
-				my $tperc	= ($total == 0) ? 'n/a' : sprintf('%.1f%%', 100*($pass/$total));
-				print qq[\t<td><span title="($pass/$run/$total)">$tperc/$rperc</span></td>\n];
-			}
-			print qq[</tr>\n];
-		}
-	}
-	print qq[</table>\n];
-}
-
-sub get_spec_totals_for_software {
-	my $results		= shift;
-	my $software	= shift;
-	my $total		= scalar(@{[ keys %{ $results->{ tests }{ required } } ]});
-	my $run			= $results->{ software }{ $software }{ required } || 0;
-	my $pass		= $results->{ software }{ $software }{ required_pass } || 0;
-	return ($total, $run, $pass);
+sub strip_test {
+	my $t	= shift;
+	$t	=~ s{http://www.w3.org/2001/sw/DataAccess/tests/data-r2/}{};
+	$t	=~ s{http://www.w3.org/2009/sparql/docs/tests/data-sparql11/}{};
+#	$t	=~ s{file:///Users/samofool/data/prog/git/perlrdf/RDF-Query/xt/dawg11/}{};
+	return $t;
 }
 
 sub print_html_head {
-	my $slist	= shift;
-	my $specs	= shift;
-	my $software	= shift;
-	
+	my $r			= shift;
 	print <<"END";
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -394,9 +198,10 @@ sub print_html_head {
 		
 END
 
-	foreach my $spec (@$slist) {
-		my $name	= $specs{ $spec }{ name };
-		print qq[\t\t<li><a href="#$name">$name</a></li>\n];
+	foreach my $spec ($r->specs) {
+		my $name	= $r->spec_name( $spec );
+		my $id		= $r->spec_id( $spec );
+		print qq[\t\t<li><a href="#$id">$name</a></li>\n];
 	}
 
 	print "</ul></li></ul>\n";
@@ -404,16 +209,9 @@ END
 
 sub print_html_foot {
 	print <<"END";
-<p class="foot">$date</class>
 </body>
 </html>
 END
 }
 
-sub strip_test {
-	my $t	= shift;
-	$t	=~ s{http://www.w3.org/2001/sw/DataAccess/tests/data-r2/}{};
-	$t	=~ s{http://www.w3.org/2009/sparql/docs/tests/data-sparql11/}{};
-#	$t	=~ s{file:///Users/samofool/data/prog/git/perlrdf/RDF-Query/xt/dawg11/}{};
-	return $t;
-}
+__END__
